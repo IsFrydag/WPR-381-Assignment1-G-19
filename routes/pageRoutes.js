0387176e9931fdf3efacbb477741a039e431e996
IsFrydag <freitagheiner3@gmail.com>
2025-05-11 15:51:16 +0200

    Added content to pages. Made some changes in the code. Got the server running.

---
+++ routes/pageRoutes.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const router = express.Router();
+
+router.get('/', (req, res) => {
+ res.render('header')
+});
+
+router.get('/about', (req, res) => {
+
+});
+
+router.get('/events', (req, res) => {
+});
+
+
+router.get('/contact', (req, res) => {
+});
+
+router.get('/thankyou', (req, res) => {
+});
+
+module.exports = router;
