9cb851d4ca53b1b643a1ff3ff96bfc1d03e5fdd9
IsFrydag <freitagheiner3@gmail.com>
2025-05-12 16:21:13 +0200

    Added information to README, slight modifications to other files

--- routes/pageRoutes.js
+++ routes/pageRoutes.js
@@ -2,21 +2,24 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/', (req, res) => {
- res.render('header')
+    res.render('home')
 });
 
 router.get('/about', (req, res) => {
-
+    res.render('about')
 });
 
 router.get('/events', (req, res) => {
+    res.render('events')
 });
 
 
 router.get('/contact', (req, res) => {
+    res.render('contact')
 });
 
 router.get('/thankyou', (req, res) => {
+    res.render('thankyou')
 });
 
 module.exports = router;
